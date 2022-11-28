a8c37bde9a0400bb773d76727199ce99efac878f
BZAgustin <abustoszanelli@gmail.com>
2022-11-28 11:43:51 -0300

    - Draft

--- knight.rb
+++ knight.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
+require './board.rb'
 
 class Knight
   attr_accessor :moves
+  include Board
 
   def initialize(moves)
     @moves = moves
   end
 
   def knight_moves(origin, destination)
-    
+    return "Destination out of bounds" unless game_board.include?(destination)
+    return "Origin out of bounds" unless game_board.include?(origin)
+
+
+  end
+
+  def build_moves
+    game_board
   end
 end
