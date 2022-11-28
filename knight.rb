d879dc37ce2e412d7e7dbde541280210d56d5dd1
BZAgustin <abustoszanelli@gmail.com>
2022-11-28 10:42:07 -0300

    - Create Knight class

---
+++ knight.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Knight
+  attr_accessor :moves
+
+  def initialize(moves)
+    @moves = moves
+  end
+
+  def knight_moves(origin, destination)
+    
+  end
+end
