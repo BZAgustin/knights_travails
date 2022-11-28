52d2aa2ce14e40814c4f0bd4026d9cbbe724a7fe
BZAgustin <abustoszanelli@gmail.com>
2022-11-28 10:41:42 -0300

    - Create 8x8 board

---
+++ board.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Board
+  attr_accessor :game_board
+
+  def initialize game_board = Array.new
+    @game_board = build_board()
+  end
+
+  def build_board
+    8.times do |i|
+      8.times do |k|
+        @game_board.push([i, k])
+      end
+    end
+  end
+end
